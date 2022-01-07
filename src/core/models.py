390ca7193a6eb2506ab2ecb2e01e8a21d53182ca
Serges Thieery <jeanpetityvelos@gmail.com>
2022-01-07 02:50:49 +0100

    creation des classes repetiteurs, jours et configuration d'un administrateur

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+# Creation de la table repetiteur
+
+class Repetiteur(models.Model):
+    nom = models.CharField(max_length=20)
+    prenom = models.CharField(max_length=20)
+    sexe = models.CharField(max_length=10)
+    dateNais = models.DateField(verbose_name="date de naissance")
+    telephone = models.IntegerField()
+    ville = models.CharField(max_length=25)
+    quartier = models.CharField(max_length=20)
+    niveau_etude = models.CharField(max_length=15)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=30)
+    photo = models.ImageField()
+
+    class Meta:
+        verbose_name = "Informations repetiteur"
+
+
+# creation de la table jours qui vas contenir les jours de la semaine
+
+class Jour(models.Model):
+    nomJour = models.CharField(max_length=8)
--- src/findRepeater/settings.py
+++ src/findRepeater/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # configuration de l'appli core
+    'core',
 ]
 
 MIDDLEWARE = [
