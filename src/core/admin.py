8f6c390c1396c6ca858de6be0459ac9985591868
Tom-michel <86242863+Tom-michel@users.noreply.github.com>
2022-01-07 14:31:01 +0100

    creation des modèles (1)

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Enseignant
+from .models import Client
+from .models import Classe
+from .models import Matiere
+from .models import Cours
+
 
 # Register your models here.
+admin.site.register(Enseignant)
+admin.site.register(Client)
+admin.site.register(Classe)
+admin.site.register(Matiere)
+admin.site.register(Cours)
--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
 
 # Create your models here.
+
+class Classe(models.Model):
+    niveau = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.niveau
+
+class Matiere(models.Model):
+    intitulé = models.CharField(max_length=200, null=True)
+
+    #relation many-to-many avec Classe
+    classeMat = models.ManyToManyField(Classe)
+
+    def __str__(self):
+        return self.intitulé
+
+class Cours(models.Model):
+    titre = models.CharField(max_length=200, null=True)
+
+    #la clé de Matiere migre vers Cours
+    matiere = models.ForeignKey(Matiere, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.titre
+
+class Enseignant(models.Model):
+    CIVILITE = (('Mr','Mr'),
+                ('Mme','Mme'),
+                ('Mle','Mle'))
+    dateNais = models.DateField(null=True)
+    niveauEtude = models.CharField(max_length=200, null=True)
+    photoProfil = models.ImageField
+    ville = models.CharField(max_length=200, null=True)
+    quartier = models.CharField(max_length=200, null=True)
+    civilité = models.CharField(max_length=200, null=True, choices=CIVILITE)
+    prenom = models.CharField(max_length=200, null=True)
+    nom = models.CharField(max_length=200, null=True)
+    telephone = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+    password = models.CharField(max_length=200, null=True)
+
+    #relation many-to-many avec Classe
+    classeEns = models.ManyToManyField(Classe)
+    #relation many-to-many avec Matiere
+    matiereEns = models.ManyToManyField(Matiere)
+
+    def __str__(self):
+        info = self.prenom+" "+self.nom
+        return info
+
+class Client(models.Model):
+    CIVILITE = (('Mr','Mr'),
+                ('Mme','Mme'),
+                ('Mle','Mle'))
+    civilité = models.CharField(max_length=200, null=True, choices=CIVILITE)
+    prenom = models.CharField(max_length=200, null=True)
+    nom = models.CharField(max_length=200, null=True)
+    telephone = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+    password = models.CharField(max_length=200, null=True)
+
+    #relation many-to-many avec Cours
+    coursCli = models.ManyToManyField(Cours)
+
+    def __str__(self):
+        info = self.prenom+" "+self.nom
+        return info
+
