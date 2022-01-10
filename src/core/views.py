88e5f752e8a8163638653834d42c936a7d70a252
Updesignn <updesign941@gmail.com>
2022-01-10 13:12:28 +0100

    modifications des pages de connexions et d'inscriptions

--- src/core/views.py
+++ src/core/views.py
@@ -1,2 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(request):
+    return render(request,'core/index.html')
+
+def inscriptionprof(request):
+    return render(request,'core/inscriptionprof.html')
+
+def inscriptionClient(request):
+    return render(request, 'core/inscriptionClient.html')
+
+def connexionprof(request):
+    return render(request, 'core/connexionprof.html')
+
+def connexionClient(request):
+    return render(request, 'core/connexionClient.html')
+
+def recherche(request):
+    return render(request, 'core/recherche.html')
