c7ed5afe6b678ea02f81a63dce1fb7f6581f4c5e
jeanpetitt <82118752+jeanpetitt@users.noreply.github.com>
2022-01-22 00:51:27 +0100

    configuration de posgresql

---
+++ findRepeater/production_settings.py
@@ -0,0 +1,16 @@
+import dj_database_url
+from .settings import *
+
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+SECRET_KEY = 'sy5d2#$j3zbxq*@dgm60--&_3=ie($$$jc=3tdqs=2739^!yqe'
+
+ALLOWED_HOSTS = ['findrepeater.herokuapp.com']
+
+DATABASES['default'] = dj_database_url.config()
+
+#MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
