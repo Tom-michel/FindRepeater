33b789bd906def453750218ae423c545202bc8f8
jeanpetitt <82118752+jeanpetitt@users.noreply.github.com>
2022-01-22 01:10:25 +0100

    stylisation de l'application deployer

--- findRepeater/production_settings.py
+++ findRepeater/production_settings.py
@@ -13,4 +13,4 @@ ALLOWED_HOSTS = ['findrepeater.herokuapp.com']
 DATABASES['default'] = dj_database_url.config()
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
