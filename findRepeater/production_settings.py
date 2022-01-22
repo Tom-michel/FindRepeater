6ed796bf98f0cbb584fabef4d9dff0b44bda8757
jeanpetitt <82118752+jeanpetitt@users.noreply.github.com>
2022-01-22 01:49:21 +0100

    creation d'une fonction variable d'environnement pour SECRET_KEY

--- findRepeater/production_settings.py
+++ findRepeater/production_settings.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 TEMPLATE_DEBUG = False
 
-SECRET_KEY = 'sy5d2#$j3zbxq*@dgm60--&_3=ie($$$jc=3tdqs=2739^!yqe'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['findrepeater.herokuapp.com']
 
